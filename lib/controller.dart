17b4b840b2865ae3cb8874963afb9f7905b79b3c
LetRicette <leticiaricette@id.uff.br>
2022-02-03 17:06:43 -0300

    código gerado usando codegen e build runner

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,18 +1,30 @@
 import 'package:mobx/mobx.dart';
+part 'controller.g.dart';
 
-class Controller {
-  var _contador = Observable(0);
-  Action? incrementar;
+class Controller = ControllerBase with _$Controller;
 
-  Controller() {
-    incrementar = Action(_incrementar);
-  }
+//a utlilização do mixin Store é para a geração dos códigos automáticos
+abstract class ControllerBase with Store {
+  @observable
+  int contador = 0;
 
-  int get contador => _contador.value;
-  set contador(int novoValor) => _contador.value = novoValor;
-
-  _incrementar() {
+  @action
+  incrementar() {
     contador++;
-    // contador.value = contador.value + 1;
   }
+
+  // var _contador = Observable(0);
+  // Action? incrementar;
+
+  // Controller() {
+  //   incrementar = Action(_incrementar);
+  // }
+
+  // int get contador => _contador.value;
+  // set contador(int novoValor) => _contador.value = novoValor;
+
+  // _incrementar() {
+  //   contador++;
+  //   // contador.value = contador.value + 1;
+  // }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -42,7 +42,7 @@ class _HomeState extends State<Home> {
                 )),
             ElevatedButton(
               onPressed: () {
-                controller.incrementar!();
+                controller.incrementar();
               },
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
